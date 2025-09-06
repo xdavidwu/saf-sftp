af420773866a17d849b1bd0ff4d3eba3e4b83ba4
Pinghao Wu <xdavidwuph@gmail.com>
2025-09-06 22:07:13 +0800

    SftpProxyFileDescriptorCallback: translate IOE to errno

--- src/main/java/link/xdavidwu/saf/sftp/SftpProxyFileDescriptorCallback.java
+++ src/main/java/link/xdavidwu/saf/sftp/SftpProxyFileDescriptorCallback.java
@@ -8,8 +8,13 @@ import android.util.Log;
 import java.io.IOException;
 
 import org.apache.sshd.sftp.client.SftpClient;
+import org.apache.sshd.sftp.common.SftpConstants;
+import org.apache.sshd.sftp.common.SftpException;
 
-public class SftpProxyFileDescriptorCallback extends ProxyFileDescriptorCallback {
+import link.xdavidwu.saf.AbstractUnixLikeProxyFileDescriptorCallback;
+
+public class SftpProxyFileDescriptorCallback
+		extends AbstractUnixLikeProxyFileDescriptorCallback {
 	private static final int OPENSSH_SFTP_MAX_MSG_LENGTH = 256 * 1024;
 	private static final int OPENSSH_SFTP_MAX_READ_LENGTH = OPENSSH_SFTP_MAX_MSG_LENGTH - 1024;
 	private static final int OPENSSH_SFTP_MAX_WRITE_LENGTH = OPENSSH_SFTP_MAX_MSG_LENGTH - 1024;
@@ -24,17 +29,44 @@ public class SftpProxyFileDescriptorCallback extends ProxyFileDescriptorCallback
 	}
 
 	@Override
-	public long onGetSize() throws ErrnoException {
-		try {
-			return sftp.stat(file).getSize();
-		} catch (IOException e) {
-			throw new ErrnoException("fstat", OsConstants.EIO);
+	protected int translateIOException(IOException e) {
+		if (e instanceof SftpException s) {
+			return switch (s.getStatus()) {
+			case SftpConstants.SSH_FX_NO_SUCH_FILE		-> OsConstants.ENOENT;
+			case SftpConstants.SSH_FX_PERMISSION_DENIED	-> OsConstants.EACCES;
+			case SftpConstants.SSH_FX_BAD_MESSAGE		-> OsConstants.EINVAL;
+			case SftpConstants.SSH_FX_NO_CONNECTION		-> OsConstants.ENOTCONN;
+			case SftpConstants.SSH_FX_CONNECTION_LOST	-> OsConstants.ENOTCONN;
+			case SftpConstants.SSH_FX_OP_UNSUPPORTED	-> OsConstants.EOPNOTSUPP;
+			case SftpConstants.SSH_FX_INVALID_HANDLE	-> OsConstants.EBADF;
+			case SftpConstants.SSH_FX_NO_SUCH_PATH		-> OsConstants.ENOENT;
+			case SftpConstants.SSH_FX_FILE_ALREADY_EXISTS -> OsConstants.EEXIST;
+			case SftpConstants.SSH_FX_WRITE_PROTECT		-> OsConstants.EPERM;
+			case SftpConstants.SSH_FX_NO_SPACE_ON_FILESYSTEM -> OsConstants.ENOSPC;
+			case SftpConstants.SSH_FX_QUOTA_EXCEEDED	-> OsConstants.EDQUOT;
+			case SftpConstants.SSH_FX_UNKNOWN_PRINCIPAL	-> OsConstants.EINVAL;
+			case SftpConstants.SSH_FX_DIR_NOT_EMPTY		-> OsConstants.ENOTEMPTY;
+			case SftpConstants.SSH_FX_NOT_A_DIRECTORY	-> OsConstants.ENOTDIR;
+			case SftpConstants.SSH_FX_INVALID_FILENAME	-> OsConstants.EINVAL;
+			case SftpConstants.SSH_FX_LINK_LOOP			-> OsConstants.ELOOP;
+			case SftpConstants.SSH_FX_INVALID_PARAMETER	-> OsConstants.EINVAL;
+			case SftpConstants.SSH_FX_FILE_IS_A_DIRECTORY -> OsConstants.EISDIR;
+			case SftpConstants.SSH_FX_OWNER_INVALID		-> OsConstants.EINVAL;
+			case SftpConstants.SSH_FX_GROUP_INVALID		-> OsConstants.EINVAL;
+			default -> OsConstants.EIO;
+			};
 		}
+		return super.translateIOException(e);
+	}
+
+	@Override
+	public long onGetSize() throws ErrnoException {
+		return io("stat", () -> sftp.stat(file).getSize());
 	}
 
 	@Override
 	public int onRead(long offset, int size, byte[] data) throws ErrnoException {
-		try {
+		return io("read", () -> {
 			Log.v("SFTP", "r: " + size + "@" + offset);
 			int doff = 0;
 			while (doff < size) {
@@ -48,9 +80,7 @@ public class SftpProxyFileDescriptorCallback extends ProxyFileDescriptorCallback
 				}
 			}
 			return size;
-		} catch (IOException e) {
-			throw new ErrnoException("read", OsConstants.EIO);
-		}
+		});
 	}
 
 	@Override
@@ -58,6 +88,6 @@ public class SftpProxyFileDescriptorCallback extends ProxyFileDescriptorCallback
 		try {
 			sftp.close(file);
 		} catch (IOException e) {
-		} // TODO
+		}
 	}
 }
